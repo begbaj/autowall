f2cd223a19cd71560f55ea977781091437a5d31e
Began <beganbajrami@gmail.com>
2022-06-16 01:39:26 +0200

    bug fix

--- autowall.py
+++ autowall.py
@@ -48,13 +48,13 @@ def main():
         API_KEY = env("AUTOWALL_API")
 
     parser = argparse.ArgumentParser(description="Simple wallpaper setter")
-    parser.add_argument("-q", type=str, nargs=1, metavar="query",
+    parser.add_argument("-q", type=str, metavar="query",
             default=None, help="Search query - Your main way of finding what you're looking for.")
-    parser.add_argument("-c", type=int, nargs=1, metavar="categories",
+    parser.add_argument("-c", type=str, metavar="categories",
             default=None, help="Turn categories on(1) or off(0). Must provide 3 bits, default is 111")
-    parser.add_argument("-p", type=int, nargs=1, metavar="purity",
+    parser.add_argument("-p", type=str, metavar="purity",
             default=None, help="Turn purities on(1) or off(0). Must provide 3 bits, default is 100")
-    parser.add_argument("-s", type=int, nargs=1, metavar="sorting",
+    parser.add_argument("-s", type=str, metavar="sorting",
             default=None, help="Method of sorting results:\n"\
                             "0 - date added (default)\n"\
                             "1 - relevance\n"\
@@ -63,21 +63,21 @@ def main():
                             "4 - favorites\n"\
                             "5 - toplist")
 
-    parser.add_argument("-o", type=int, nargs=1, metavar="order",
+    parser.add_argument("-o", type=str, metavar="order",
 					default=None, help="Sorting order: 0 - desc (default); 1 - asc")
 
-    parser.add_argument("-r", type=str, nargs=1, metavar="resolution",
+    parser.add_argument("-r", type=str, metavar="resolution",
 					default=None, help="Minimum resolution allowed")
 
-    parser.add_argument("--exact-res", type=str, nargs=1, metavar="exact_resolution",
+    parser.add_argument("--exact-res", type=str, metavar="exact_resolution",
 					default=None, help="")
 
-    parser.add_argument("--seed", type=str, nargs=1, metavar="seed",
+    parser.add_argument("--seed", type=str, metavar="seed",
 					default=None, help="Optional seed for random results")
 
-    parser.add_argument("-R", type=str, nargs=1, metavar="ratio",
+    parser.add_argument("-R", type=str, metavar="ratio",
 					default="16x9", help="Aspect ratio (default: 16x9)")
-    parser.add_argument("-C", type=str, nargs=1, metavar="color",
+    parser.add_argument("-C", type=str, metavar="color",
 					default=None, help="Search by color.")
     parser.add_argument("-v", action=argparse.BooleanOptionalAction)
 
@@ -94,13 +94,15 @@ def main():
         http_args += f"&categories={str(args.c).zfill(3)}"
 
     if args.p is not None:
+        log.debug(args.p)
         if str(args.p)[-1] == "1":
             if API_KEY is None:
                 log.error("no api key provided")
                 print("you must provide a API key to get NSFW content")
                 return
             http_header = {"X-API-Key":f"{API_KEY}"}
-        http_args += f"&purity={str(args.p[0]).zfill(3)}"
+            log.info("api key setted")
+        http_args += f"&purity={str(args.p).zfill(3)}"
 
     if args.s is not None:
         if args.s[0] == 1:
