013c8e0e23f35c404ca35bf6af25f966ee6899e5
Began <beganbajrami@gmail.com>
2023-09-29 10:12:59 +0200

    feat: --use --keep --download and --list commands refactor: README.md updated and removed unused code and directories

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Autowall
+Autowall is a very simple script to automatically download and set wallpapers from [Wallhaven.cc](https://wallhaven.cc/)
 
-TODO: a decent readme
+## Installation
+Just clone and run ` sh install.sh `.
+
+```bash
+git clone https://github.com/begbaj/autowall
+cd autowall
+sh install.sh
+```
+
+## LICENSE
+This software is distributed under the GPLv3 license.
--- autowall.py
+++ autowall.py
@@ -19,6 +19,7 @@ from os import getenv as env
 from os import path
 from os import mkdir
 from os import system
+from os import listdir
 from sys import argv
 
 ALLOW = False
@@ -27,20 +28,15 @@ PROVIDER_HINT = ""
 AVAILABLE_PROVIDERS = ["wallheaven"]
 URL_SEARCH = "https://wallhaven.cc/api/v1/search"
 API_KEY = None
+autodir = path.expanduser("~/.local/share/autowall/")
 
 def main():
-    global ALLOW
-    global PROVIDER
-    global PROVIDER_HINT
-    global AVAILABLE_PROVIDERS
-    global URL_SEARCH
-    global API_KEY
+    log.basicConfig(level=log.ERROR)
+
     if len(argv) == 1:
-        ALLOW = True
-        print("No arguments passed")
+        print("No arguments passed", 1)
         return
 
-    log.basicConfig(level=log.ERROR)
     if env("AUTOWALL_API") is None:
         log.warning("No api key provided!")
     else:
@@ -79,37 +75,90 @@ def main():
 					default="16x9", help="Aspect ratio (default: 16x9)")
     parser.add_argument("-C", type=str, metavar="color",
 					default=None, help="Search by color.")
-    parser.add_argument("-v", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-v", help="Verbose output", action=argparse.BooleanOptionalAction)
     parser.add_argument("--random", help="select a random result", action=argparse.BooleanOptionalAction)
-    parser.add_argument("--keep", type=bool,action=argparse.BooleanOptionalAction, help="Set the last wallpeper downloaded")
+
+    parser.add_argument("--use-last", type=bool,action=argparse.BooleanOptionalAction, help="Set the last wallpeper downloaded")
+    parser.add_argument("-u","--use", default=None, type=str, help="Set one of the previously downloaded wallpapers")
+    parser.add_argument("-l","--list-all", type=bool,action=argparse.BooleanOptionalAction, help="List downloaded wallpapers")
+    parser.add_argument("--keep", type=str, help="Save current wallpaper")
 
     args = parser.parse_args()
-
-    http_args=""
-    http_header = None
-    
-    if args.keep:
-        set_wallpaper(keep=True)
-        return
+    r = None
+    neww = False
 
     if args.v is not None:
         ALLOW = True
+
+    if args.use_last:
+        setw()
+        return
+
+    if args.use is not None:
+        filename = "paper"
+        if args.use != "":
+            filename = args.use
+        try:
+            setw(filename)
+        except FileNotFoundError:
+            print(f"File was not found in {autodir}/{filename}")
+        finally:
+            return
+    # todo: list available papers
+    if args.list_all:
+        files = listdir(autodir)
+        for file in files:
+            print(file, 1)
+        return
+    if args.keep is not None:
+        if args.keep == "":
+            print("what should we name it?", 1)
+        else:
+            paper = f"{autodir}paper"
+            name = f"{autodir}{args.keep}"
+            shutil.copy(paper, name)
+            if path.exists(name):
+                print(f"wallpaper successfully saved to {name}", 1)
+            else:
+                print(f"something went wrong :/", 1)
+        return
+
+    if args.q is not None or args.c is not None or args.p is not None or \
+    args.s is not None or args.o is not None or args.r is not None or \
+    args.c is not None or args.R is not None or args.C is not None:
+        neww = True
+        (query, header) = url_composer(args)
+        r = requests.get(query,  headers=header)
+        jsonres = r.json()
+        lenjson = len(jsonres["data"])
+        if lenjson == 0:
+            print("no results found")
+            return
+        result = 0
+        if args.random:
+            result = random.randint(0, lenjson)
+        url = r.json()["data"][result]["path"]
+        download(url)
+        setw()
+
+def url_composer(args) -> tuple:
+    http_args=""
+    http_header = None
+
     if args.q is not None:
         http_args += f"q={args.q}"
     if args.c is not None:
         http_args += f"&categories={str(args.c).zfill(3)}"
-
     if args.p is not None:
         log.debug(args.p)
         if str(args.p)[-1] == "1":
             if API_KEY is None:
                 log.error("no api key provided")
                 print("you must provide a API key to get NSFW content")
-                return
+                exit(-1)
             http_header = {"X-API-Key":f"{API_KEY}"}
             log.info("api key setted")
         http_args += f"&purity={str(args.p).zfill(3)}"
-
     if args.s is not None:
         if args.s == "1":
             http_args += "&sorting=relevance"
@@ -121,61 +170,42 @@ def main():
             http_args += "&sorting=favorites"
         elif args.s == "5":
             http_args += "&sorting=toplist"
-
     if args.o is not None:
         if args.o == 1:
             http_args += "&order=asc"
-
     if args.r is not None:
         http_args += f"&atleast={args.r}"
-
     if args.exact_res is not None and args.r is None:
         http_args += f"&resolution={args.exact_res[0]}"
-
     if args.seed is not None:
         http_args += f"&seed={args.seed}"
-
     if args.c is not None:
         http_args += f"&categories={args.c}"
-
     if args.R is not None:
         http_args += f"&ratios={args.R}"
-
     if args.C is not None:
         http_args += f"&colors={args.C}"
 
     search_query = URL_SEARCH + "/?" + quote(http_args, "?=&")
-    r = requests.get(search_query,  headers=http_header)
-    print(search_query)
-    jsonres = r.json()
-    lenjson = len(jsonres["data"])
-    if lenjson == 0:
-        print("no results found")
-        return
-    result = 0
-    if args.random:
-        result = random.randint(0, lenjson)
-    url = r.json()["data"][result]["path"]
-    print(url)
-    download_and_set(url)
+    return (search_query, http_header)
 
-def download_and_set(url):
+def download(url):
     # DOWNLOAD
     img_raw = requests.get(url, stream=True).raw
     img_raw.decode_content = True
-    set_wallpaper(img_raw, img_file)
+    if not path.isdir(autodir):
+       mkdir(autodir) 
+    with open(autodir + "paper", "wb") as img_file:
+        shutil.copyfileobj(img_raw, img_file)
 
-def set_wallpaper(img_raw=None, keep=False):
-    autodir = path.expanduser("~/.local/share/autowall/")
-    if not keep:
-        if not path.isdir(autodir):
-           mkdir(autodir) 
-        with open(autodir + "paper", "wb") as img_file:
-            shutil.copyfileobj(img_raw, img_file)
-    system(f"feh --no-fehbg --bg-scale {autodir}/paper")
+def setw(filename="paper"):
+    try:
+        system(f"feh --no-fehbg --bg-scale {autodir}/{filename}")
+    except:
+        raise FileNotFoundError
 
-def print(msg):
-    if ALLOW:
+def print(msg, bypass=0):
+    if ALLOW or bypass == 1:
         __builtin__.print(msg)
 
 if __name__ == "__main__":
